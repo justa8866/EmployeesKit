b56adf176ad063468282e6f922c80a4673f122e8
Justyna Duranowska <justa8866@gmail.com>
2023-03-12 00:22:24 +0100

    feat(toastify): add react-toastify

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,19 @@ import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-import 'react-toastify/dist/ReactToastify.css';
-import { LinearProgress } from '@mui/material';
-import { ThemeProvider } from '@mui/system';
-import theme from './theme/theme';
+import 'react-toastify/dist/ReactToastify.css'
+import { LinearProgress } from '@mui/material'
+import { ThemeProvider } from '@mui/system'
+import { ToastContainer } from 'react-toastify'
+import theme from './theme/theme'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-  <ThemeProvider theme={theme}>
-    <Suspense fallback={<LinearProgress />}>
-      <App />
-    </Suspense>
-  </ThemeProvider>
-</React.StrictMode>,
+    <ThemeProvider theme={theme}>
+      <Suspense fallback={<LinearProgress />}>
+        <App />
+        <ToastContainer />
+      </Suspense>
+    </ThemeProvider>
+  </React.StrictMode>,
 )
