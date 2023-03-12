778fa32790787c6da138c5bc9aa5441b9401b3f5
Justyna Duranowska <justa8866@gmail.com>
2023-03-12 05:50:07 +0100

    feat(statistic): add top 5 employee with largest number of completed task stat

---
+++ src/components/StatisticsEmployee/index.tsx
@@ -0,0 +1,97 @@
+import React, { useEffect, useState } from 'react'
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from '@mui/material'
+import moment from 'moment'
+
+import { getEmployees, getIdFromPath } from '../Employee/services/employee.service'
+import { IEmployee } from '../Employee/types/IEmployee'
+
+import { getTasks } from '../Task/services/task.service'
+
+interface IOccurency {
+  [key: string]: number
+}
+
+const StatisticsEmployee = () => {
+  const [employeesList, setEmployeesList] = useState<IEmployee[]>([])
+  const [employeesWithLargestNumberOfCompletedTask, setEmployeesWithLargestNumberOfCompletedTask] =
+    useState<IEmployee[]>([])
+
+  useEffect(() => {
+    const getEmployee = async () => {
+      const list = await getEmployees()
+
+      setEmployeesList(list)
+    }
+
+    getEmployee()
+  }, [])
+
+  useEffect(() => {
+    getEmployeesWithLargestNumberOfCompletedTask()
+  }, [employeesList])
+
+  const getEmployeesWithLargestNumberOfCompletedTask = async () => {
+    const tasksList = await getTasks()
+
+    const occurencies: IOccurency = {}
+
+    for (const task of tasksList) {
+      if (!task.assignee) {
+        continue
+      }
+
+      const id = getIdFromPath(task.assignee)
+      occurencies[id] = occurencies[id] ? occurencies[id] + 1 : 1
+    }
+
+    const sortedTopFiveElements = Object.entries(occurencies)
+      .sort((a, b) => b[1] - a[1])
+      .slice(0, 5)
+      .map(item => item[0])
+
+    setEmployeesWithLargestNumberOfCompletedTask(
+      employeesList.filter(employee => sortedTopFiveElements.includes(employee.id || '')),
+    )
+  }
+
+  return (
+    <TableContainer component={Paper}>
+      <Table aria-label='employee table'>
+        <TableHead>
+          <TableRow>
+            <TableCell>id</TableCell>
+            <TableCell align='right'>Full Name</TableCell>
+            <TableCell align='right'>E-mail</TableCell>
+            <TableCell align='right'>Phone</TableCell>
+            <TableCell align='right'>Date of birth</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {employeesWithLargestNumberOfCompletedTask.map((employee: IEmployee, index: number) => (
+            <TableRow key={index} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+              <TableCell component='th' scope='row'>
+                {employee.id}
+              </TableCell>
+              <TableCell>{employee.fullName}</TableCell>
+              <TableCell align='right'>{employee.email}</TableCell>
+              <TableCell align='right'>{employee.phoneNumber}</TableCell>
+              <TableCell align='right'>
+                {moment(employee.brithDate.toDate()).format('MM / DD / YYYY')}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+}
+
+export default StatisticsEmployee
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -4,6 +4,7 @@ import LayoutGrid from '../../common/LayoutGrid'
 import { ModalProvider } from '../../common/Modal/context'
 import NavProvider from '../../components/Navbar/Reducer/NavReducer'
 import Navbar from '../../components/Navbar'
+import StatisticsEmployee from '../../components/StatisticsEmployee'
 
 const HomePage = () => {
   return (
@@ -12,7 +13,9 @@ const HomePage = () => {
         <Navbar />
       </NavProvider>
       <LayoutGrid>
-        <Grid container mt={10}></Grid>
+        <Grid container mt={10}>
+          <StatisticsEmployee />
+        </Grid>
       </LayoutGrid>
     </ModalProvider>
   )
